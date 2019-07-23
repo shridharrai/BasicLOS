b3d5978554be8c28427c97453e088247285ae5f5
Shridhar Rai <shridharrai3@gmail.com>
2019-07-23 22:05:11 +0530

    Add files via upload

---
+++ src/com/brainmentors/los/start/Application.java
@@ -0,0 +1,27 @@
+package com.brainmentors.los.start;
+
+import com.brainmentors.los.operation.LOSProcess;
+import com.brainmentors.los.utils.Utility;
+
+public class Application {
+public static void main(String[] args) {
+	LOSProcess process=new LOSProcess();
+	while(true){
+	System.out.println("Do you have Application Number or NOt(Enter 0) Press -1 to Exit");
+	int applicationNumber=Utility.scanner.nextInt();
+	if(applicationNumber==-1) {
+		System.out.println("Thanks for Using");
+		System.exit(0);
+	}
+	if(applicationNumber==0) {
+//		New Customer
+		process.sourcing();
+	}
+	
+	else {
+//		Existing Customer
+		process.checkStage(applicationNumber);
+	}
+}
+}
+}
